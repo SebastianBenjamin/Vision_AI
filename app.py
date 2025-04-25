7ae743e918c0d51d16f46c0b1cf59ab9706e75b0
Benjamin Sebastian <142651700+SebastianBenjamin@users.noreply.github.com>
2025-04-25 21:18:15 +0530

    updations in app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
+from PIL import Image  # Add this line at the top with other imports
 from model import ImageCaptioningModel
 import time
 
